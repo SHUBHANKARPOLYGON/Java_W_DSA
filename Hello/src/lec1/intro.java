f6d5744d1c749fd1a8eed00d3b373697069638c6
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-06-24 12:46:04 +0530

    Intro classes

---
+++ Hello/src/lec1/intro.java
@@ -0,0 +1,90 @@
+package lec1;
+
+public class intro {
+
+	public static void main(String[] args) {
+		
+		
+		//normal sum
+//		int a=5;
+//		int b=2;
+//		
+//		int sum=a+b;
+//		System.out.println(sum);
+		
+		
+		//if-else
+//		double speed=80;
+//		if(speed>100) {
+//			System.out.println("overspeed");
+//		}
+//		else {
+//			System.out.println("safe");
+//		}
+		
+		
+		//if-else multiple else-if
+//		int age=12;
+//		
+//		if(age<10) {
+//			System.out.println("not sit on front seat");
+//		}
+//		else if(age>=10 && age<16) {
+//			System.out.println("can sit in front seat but not drive");
+//		}
+//		else if (age>=16 && age<18) {
+//			System.out.println("can opt for diving licence");
+//		}
+//		else {
+//			System.out.println("Drive");
+//		}
+		
+		
+		//Nested if
+//		char visa = 'A';
+//		int amt = 60000;
+//		
+//		if (visa=='A') {
+//			if(amt>=100000) {
+//				System.out.println("Ultra Rich");
+//			}
+//			else if(amt<100000 && amt>=50000) {
+//				System.out.println("rich");
+//			}
+//			else {
+//				System.out.println("avg");
+//			}
+//		}
+		
+		//while loop
+//		int i=1;
+//		while(i<=100) {
+//				System.out.println(i);
+//			i=i+1;
+//		}
+		
+		//While loop even
+//		int i=1;
+//		while(i<=100) {
+//			if(i%2==0)
+//				System.out.println(i);
+//			i=i+1;
+//		}
+		
+		
+		//Table from 2 to 5 using while loop
+//		int i=2;
+//		int e=5;
+//		while(i<=e) {
+//			int j=1;
+//			while(j<=10) {
+//				System.out.println(i*j);
+//				j++;
+//			}
+//			i++;
+//		}
+		
+		
+
+}
+}
