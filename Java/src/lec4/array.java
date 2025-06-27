d61c7e6c3421eb764e1db3deb40100c092e01ec0
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-06-27 11:49:37 +0530

    array

---
+++ Java/src/lec4/array.java
@@ -0,0 +1,55 @@
+package lec4;
+
+import java.util.Scanner;
+
+public class array {
+
+	public static void main(String[] args) {
+		
+		
+		//normal arrar ques
+		
+//		Scanner sc=new Scanner(System.in);
+//		int size=sc.nextInt();
+//		
+//		int arr[]=new int[size];
+//		
+//		for(int i=0;i<=size-1;i++) {
+//			arr[i]=sc.nextInt(); //come array in input
+//		}
+//		for(int i=0;i<=size-1;i++) {
+//			System.out.print(arr[i]+" "); 
+//		}
+		
+		
+		//sum of array
+		
+//		Scanner sc=new Scanner(System.in);
+//		int size=sc.nextInt();
+//		
+//		int arr[]=new int[size];
+//		
+//		for(int i=0;i<=size-1;i++) {
+//			arr[i]=sc.nextInt();
+//		}
+//		
+//		System.out.println();
+//	int sum=0;
+//	for(int i=0;i<=size-1;i++) {
+//		sum=sum+arr[i];
+//		System.out.println(sum);
+//	}
+
+
+		//array element searching
+		
+		Scanner sc=new Scanner(System.in);
+		int size=sc.nextInt();
+		
+		int arr[]=new int[size];
+		
+		
+		
+}
+
+}
---
+++ Java/src/lec4/celciustofarenheit.java
@@ -0,0 +1,66 @@
+package lec4;
+
+import java.util.Scanner;
+
+public class celciustofarenheit {
+
+	public static void main(String[] args) {
+		
+		//celcius to farenheit
+		
+//		double c,f;
+//				
+//		for(int i=0;i<=100;i=i+2);{
+//			double i = 18;
+//			f=(int)((i*1.8)+32);
+//			System.out.println(i+"->"+f);
+//			
+//		}
+
+		
+		
+		//Abudant number
+		
+//		Scanner sc=new Scanner(System.in);
+//		int n=sc.nextInt();
+//		
+//		int factSum=1;
+//		
+//		for(int i=2;i<n;i++) {
+//			if(n%i==0)factSum=factSum+i;
+//		}
+//		
+//		System.out.println(n==factSum?"Neutal":n>factSum?"Deficiet number":"abundant");
+		
+		
+		
+		//odd even car rule in Delhi
+		
+//		Scanner sc=new Scanner(System.in);
+//		int n= sc.nextInt();
+//		
+//		int odd=0;
+//		int even=0;
+//		
+//		while(n>0) {
+//			int d=n%10;
+//			if(d%2==0) even=even+d;
+//			
+//				else odd=odd+d;
+//				
+//			n=n/10;
+//			}
+//			if(odd%3==0 || even%4==0) {
+//				System.out.println("Ride");
+//			}
+//			else {
+//				System.out.println("not Ride");
+//			}
+		
+		
+		
+		//Binary to decimal conversion
+		
+		}
+	}
+
