7ab40381fa3bc290c274809cfaa7eb7895fbac7e
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-06-27 12:39:59 +0530

    array 2nd

--- Java/src/lec4/array.java
+++ Java/src/lec4/array.java
@@ -41,12 +41,45 @@ public class array {
 //	}
 
 
-		//array element searching
+		//array max min element searching
 		
-		Scanner sc=new Scanner(System.in);
-		int size=sc.nextInt();
+//		int arr[]= {3,2,4,5,71,5};
+//		
+//		int min=Integer.MAX_VALUE; //dont know the minimum
+//		int max=Integer.MIN_VALUE; //dont know the maximum
+//		
+//		for(int i=0;i<arr.length;i++) {
+//			if(max<arr[i]) {
+//				max=arr[i];
+//			}
+//			if(min>arr[i])min=arr[i];
+//		}
+//		System.out.println(min+" "+max);
 		
-		int arr[]=new int[size];
+		
+		
+		//two sum array
+		
+		int arr[]= {1,2,3,4,5};
+		int target=5;
+		int temp=0;
+		
+		for(int i=0;i<arr.length;i++) {
+			
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i]+arr[j]==target) {
+					System.out.println(i+" "+j);
+					temp=1;
+					break;
+				}
+				
+			}
+			if(temp==1)break;
+			
+		}
+		
+		if(temp==0)
+			System.out.println("target not found");
 		
 		
 		
