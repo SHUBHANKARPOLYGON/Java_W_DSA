fc7a0401aff8e4b178798541ad2f1e63523c0d0b
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-06-30 12:26:19 +0530

    subarray defined

---
+++ Java/src/lec5/subarray.java
@@ -0,0 +1,24 @@
+package lec5;
+
+
+public class subarray {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {1,4,-4,5,-10,1};
+		
+		int max=Integer.MIN_VALUE;
+		int sum=0;
+		for(int i=0;i<arr.length;i++) {
+			sum=sum+arr[i];
+			if(sum>max) {
+				max=sum;
+			}
+			if(sum<0) {
+				sum=0;
+			}
+		}
+		System.out.println(max);
+	}
+
+}
