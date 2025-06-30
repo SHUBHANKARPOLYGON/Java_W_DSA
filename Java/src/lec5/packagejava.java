bf4d77f87ed1cad7a28b70842f33b601b4a2f506
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-06-30 12:03:07 +0530

    package complete

---
+++ Java/src/lec5/packagejava.java
@@ -0,0 +1,92 @@
+package lec5;
+
+public class packagejava {
+
+	public static void main(String[] args) {
+	//calling a functions in main	
+//		fun1();
+//		System.out.println(sq(5));
+		
+		//swap of arr called in functions
+//		int arr[]= {2,3,4,5,6,7};
+//		swap(arr);
+//		for(int i=0;i<arr.length;i++) {
+//			System.out.print(arr[i]+" ");
+//		}
+		
+//		Reverse of array called in main
+		
+//		int arr[]= {1,2,3,4,5,6,7};
+//		reverse(arr);
+//		for(int i=0;i<arr.length;i++) {
+//			System.out.print(arr[i]+" ");
+//		}
+		
+//	}
+	
+		
+		//list of array called in function
+//	int arr[]= {3,1,4,-1,7,21,1};
+//	int target=4;
+//	boolean found = Search(arr,target);
+//	System.out.println("Is " + target + " in the array? " + found);
+//	
+//	for(int i=0;i<arr.length;i++) {
+//		System.out.print(arr[i]+" ");
+//	}
+		
+		
+		
+		
+}
+	
+	
+	//Defining a function
+//	public static void fun1() {
+//		System.out.println("Hello");
+//	}
+//	
+//	public static int sq(int a) {
+//		return(a*a);
+//	}
+	
+	//creating functions of swap
+//	public static void swap(int arr[]) {
+//		int temp = arr[1];
+//		arr[1]=arr[4];
+//		arr[4]=temp;
+//}
+
+	
+	//Reverse of array using functions
+	
+//	public static void reverse(int arr[]) {
+//		int start=0;
+//		int end=arr.length-1;
+//		
+//		while(start<end) {
+//			int temp=arr[start];
+//			arr[start]=arr[end];
+//			arr[end]=temp;
+//			start++;
+//			end--;
+//		}
+//		
+//	}
+	
+	
+	//Find the no. which is available in the list of array
+	
+//	public static boolean Search(int arr[], int target) {
+//		for(int i=0;i<arr.length;i++) {
+//			if(arr[i]==target) return true;
+//		}
+//		return false;
+//	}
+	
+	
+	
+	
+}
+	
+
