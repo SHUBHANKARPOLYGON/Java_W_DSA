b6cafd2d6ee9cc1f09833c8af9d483e4a500550e
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-07-07 12:16:03 +0530

    aray freq

--- Java/src/lec10/arrayfreq.java
+++ Java/src/lec10/arrayfreq.java
@@ -4,20 +4,30 @@ public class arrayfreq {
 
 	public static void main(String[] args) {
 		
-		String s="kalakaliya";
+//		String s="kalakaliya";
+//		
+//		int arr[]=new int[26];
+//		for(int i=0;i<s.length();i++) {
+//			int idx=s.charAt(i)-'a';
+//			
+//			arr[idx]++;
+//		}
+//		for(int i=0;i<arr.length;i++) {
+//			if(arr[i]!=0) {
+//				System.out.println((char)('a'+i)+" "+arr[i]);
+//			}
+//		}
 		
-		int arr[]=new int[26];
-		for(int i=0;i<s.length();i++) {
-			int idx=s.charAt(i)-'a';
-			
-			arr[idx]++;
-		}
-		for(int i=0;i<arr.length;i++) {
-			if(arr[i]!=0) {
-				System.out.println((char)('a'+i)+" "+arr[i]);
-			}
-		}
-		
+		// Count capital Characters
+		 String s = "abGdjEDJdkkdykGIEGOjo";
+	        int count = 0;
+	        for(int i= 0;i<s.length();i++) {
+	            char c= s.charAt(i);
+	            if(c>='A' && c<='Z') {
+	                count++;
+	            }
+	        }
+	        System.out.println("Number of capital characters: " +count);
 	}
 
 }
