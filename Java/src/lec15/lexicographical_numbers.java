eda6f826009989f9076782d004ac633aa8827d56
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-07-14 16:00:47 +0530

    lec 15 complete

---
+++ Java/src/lec15/lexicographical_numbers.java
@@ -0,0 +1,23 @@
+package lec15;
+
+public class lexicographical_numbers {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=9;i++) {
+			create(i,114);
+		}
+
+	}
+	public static void create(int n,int given_n) {
+		if(n>given_n)
+			return;
+		System.out.println(n+"");
+		n=n*10;
+		if(n>given_n)
+			return;
+		for(int i=0;i<=9;i++) {
+			create(n+i,given_n);
+		}
+	}
+
+}
