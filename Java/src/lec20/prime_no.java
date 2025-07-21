eae98145173cfd4e73e16f4651c82c3d33840c0b
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-07-21 14:40:49 +0530

    prime no

---
+++ Java/src/lec20/prime_no.java
@@ -0,0 +1,21 @@
+package lec20;
+
+public class prime_no {
+	
+	//Time Complexities
+	public static void main(String[] args) {
+		for(int i=2;i<=100000;i++) {
+			if(Prime(i)==true) {
+				System.out.println(i);
+			}
+		}
+	}
+	public static boolean Prime(int n) {
+		for(int i=2;i<=(n-1);i++) {
+			if(n%i==0)
+				return false;
+		}
+		return true;
+	}
+
+}
