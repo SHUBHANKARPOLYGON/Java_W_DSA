72b1af435d1b22397f8c242303b2057366e00bd9
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-07-23 14:35:16 +0530

    stack lec22

---
+++ Java/src/lec22/queue.java
@@ -0,0 +1,24 @@
+package lec22;
+
+public class queue {
+	
+	private int arr[];
+	private int front[];
+	private int size;
+	
+public queue() {
+		arr=new int [5];
+		front=0;
+		int size=0;
+	}
+	
+	public boolean isEmpty() {
+		return size==0;
+	}
+	public boolean isFull() {
+		return size==arr.length;
+	}
+	public void Enqueue() {
+		
+	}
+}
---
+++ Java/src/lec22/stack_client.java
@@ -0,0 +1,14 @@
+package lec22;
+
+public class stack_client {
+
+    public static void main(String[] args) throws Exception{
+        stack s = new stack(2);
+        // System.out.println(s.isEmpty());
+        s.push(1);
+        s.push(2);
+        System.out.println(s.peek());
+        // s.Display();
+        // System.out.println(s.isFull());
+    }
+}
